dd46afd439c6c0a3cb63f3fc129275a50cc3985a
Yonic <yonicstudios@gmail.com>
2020-01-13 22:13:12 +0100

    Add alternative Qt translation files

--- DoorRandomizer/main.cpp
+++ DoorRandomizer/main.cpp
@@ -17,18 +17,14 @@ int main(int argc, char *argv[])
     locale.truncate(locale.lastIndexOf("_"));
 
     QString languagepacks_path = QCoreApplication::applicationDirPath().append("/LanguagePacks/");
-
-    #ifdef WIN32
-    QString translation_file = QString("qt_%1.qm").arg(locale);
-    #else
-    // We don't know why, but qt_LOCALE.qm files can't be opened in Linux
-    // so we load qtbase_LOCALE.qm instead
     QString translation_file = QString("qtbase_%1.qm").arg(locale);
-    #endif
+    QString alt_translation_file = QString("qt_%1.qm").arg(locale);
 
     if(qt_translator.load(qt_translations_path + translation_file))
         a.installTranslator(&qt_translator);
     else if(qt_translator.load(qt_trans_system_path + translation_file))
+        a.installTranslator(&qt_translator);
+    else if(qt_translator.load(qt_trans_system_path + alt_translation_file))
         a.installTranslator(&qt_translator);
     if(app_translator.load(languagepacks_path + QString("mpdr_%1.qm").arg(locale))) {
         a.installTranslator(&app_translator);
